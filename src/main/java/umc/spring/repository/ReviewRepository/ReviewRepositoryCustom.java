3aeba793ed3f2882f869d11246ab8f52b63bea4e
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-11-22 02:02:35 +0900

    Feat: Review DTO 구현

---
+++ src/main/java/umc/spring/repository/ReviewRepository/ReviewRepositoryCustom.java
@@ -0,0 +1,4 @@
+package umc.spring.repository.ReviewRepository;
+
+public interface ReviewRepositoryCustom {
+}
---
+++ src/main/java/umc/spring/repository/ReviewRepository/ReviewRepositoryImpl.java
@@ -0,0 +1,4 @@
+package umc.spring.repository.ReviewRepository;
+
+public class ReviewRepositoryImpl {
+}
