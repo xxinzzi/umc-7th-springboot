0f3f73f31e220aeb0f3b2f0c504c4281f4c0a97a
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-12-14 03:40:53 +0900

    Feat: 유저가 작성한 리뷰 조회 api 구혐

--- src/main/java/umc/spring/service/ReviewService/ReviewQueryServiceImpl.java
+++ src/main/java/umc/spring/service/ReviewService/ReviewQueryServiceImpl.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 import umc.spring.domain.Review;
 import umc.spring.repository.RestaurantRepository.RestaurantRepository;
 import umc.spring.repository.ReviewRepository.ReviewRepository;
+import umc.spring.repository.UserRepository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewQueryServiceImpl implements ReviewQueryService{
     private final ReviewRepository reviewRepository;
     private final RestaurantRepository restaurantRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public Page<Review> getReviewList(Long restaurantId, Integer page) {
+    public Page<Review> getRestaurantReviewList(Long restaurantId, Integer page) {
         // 가게 존재 여부 확인
         restaurantRepository.findById(restaurantId)
                 .orElseThrow(() -> new IllegalArgumentException("Restaurant not found"));
@@ -25,4 +27,18 @@ public class ReviewQueryServiceImpl implements ReviewQueryService{
 
         return reviewRepository.findAllByRestaurantId(restaurantId, pageRequest);
     }
+
+    @Override
+    public Page<Review> getUserReviewList(Long userId, Integer page) {
+        // 유저 존재 여부 확인
+        userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        // 페이징 처리
+        PageRequest pageRequest = PageRequest.of(page, 10);
+
+        return reviewRepository.findAllByUserId(userId, pageRequest);
+    }
+
+
 }
--- src/main/java/umc/spring/web/controller/ReviewRestController.java
+++ src/main/java/umc/spring/web/controller/ReviewRestController.java
@@ -17,6 +17,7 @@ import umc.spring.domain.Review;
 import umc.spring.service.ReviewService.ReviewCommandService;
 import umc.spring.service.ReviewService.ReviewQueryService;
 import umc.spring.validation.annotation.ExistRestaurants;
+import umc.spring.validation.annotation.ExistUsers;
 import umc.spring.web.dto.Restuarant.RestaurantRequestDTO;
 import umc.spring.web.dto.Restuarant.RestaurantResponseDTO;
 import umc.spring.web.dto.Review.ReviewResponseDTO;
@@ -36,7 +37,7 @@ public class ReviewRestController {
         return ApiResponse.onSuccess(ReviewConverter.toReviewResultDto(review));
     }
 
-    @GetMapping("/{restaurantId}")
+    @GetMapping("/restaurant/{restaurantId}")
     @Operation(summary = "특정 가게의 리뷰 목록 조회 API", description = "특정 가게의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
@@ -49,7 +50,32 @@ public class ReviewRestController {
     })
     public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getReviewList(@ExistRestaurants @PathVariable(name = "restaurantId") Long restaurantId, @RequestParam(name = "page") Integer page){
         // Service 호출로 reviewList 값을 반환받음
-        Page<Review> reviewList = reviewQueryService.getReviewList(restaurantId, page);
+        Page<Review> reviewList = reviewQueryService.getRestaurantReviewList(restaurantId, page);
+
+        // DTO로 변환
+        ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO = ReviewConverter.reviewPreViewListDTO(reviewList);
+
+        // 성공 응답 반환
+        return ApiResponse.onSuccess(reviewPreViewListDTO);
+    }
+
+    @GetMapping("/user/{userId}")
+    @Operation(summary = "유저가 작성한 리뷰 목록 조회 API", description = "유저가 작성한 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String으로 page 번호를 주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "유저의 아이디, path variable 입니다!")
+    })
+    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getUserReviewList(
+            @ExistUsers @PathVariable(name = "userId") Long userId,
+            @RequestParam(name = "page") Integer page) {
+
+        // Service 호출로 reviewList 값을 반환받음
+        Page<Review> reviewList = reviewQueryService.getUserReviewList(userId, page);
 
         // DTO로 변환
         ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO = ReviewConverter.reviewPreViewListDTO(reviewList);
